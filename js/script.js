292b035c69c467860aba9dc752f6b4bb81dd92f8
DarkDeath2796 <gelepukinandrew@gmail.com>
2025-08-06 03:14:25 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,16 +2,16 @@ const button = document.getElementById('translate-button')
 
 async function translate() {
   const text = document.getElementById('orig').value;
+
+  button.disabled = true;
   
   try {
     const res = await fetch('https://uselessfacts.jsph.pl/api/v2/facts/random?language=en');
     const data = await res.json();
-    document.getElementById('paj').value = 'Did you know that... ' + data.text;
+    document.getElementById('paj').value = `Did you know that... ${data.text}`;
   } catch (e) {
     document.getElementById('paj').value = 'Translating...';
   }
-
-  button.disabled = true;
 
   try {
     const response = await fetch('https://pat317495.onrender.com/api/translate', {
